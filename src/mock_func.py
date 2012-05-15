8880684ce1f4b95a244a44889d7f5c32d5a270f9
Chris Loew <cloew@mymail.mines.edu>
2012-05-14 19:19:47 -0600

    Added first steps to INteractive Coding

---
+++ src/mock_func.py
@@ -0,0 +1,10 @@
+
+
+class MockFunc:
+    """ Mocks a function """
+    
+    def __init__(self, file, func):
+        """  """
+        self.vars = []
+        self.code = []
+    
---
+++ src/test_func.py
@@ -0,0 +1,15 @@
+
+
+def test_func():
+    """  """
+    a = 0
+    
+    a += 1
+    
+    a += 2
+    
+    a += 3
+    
+    
+if __name__ == "__main__":
+    test_func()
