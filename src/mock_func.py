144e7ad519275ae88960b2cdbadf01be9c5f7460
Rory Olsen <roris.solaris@gmail.com>
2012-05-14 20:07:19 -0600

    Adding regex to detect function

--- src/mock_func.py
+++ src/mock_func.py
@@ -1,24 +1,28 @@
-
+import re
 
 class MockFunc:
     """ Mocks a function """
-    
+
     def __init__(self, filename, func):
         """  """
         self.vars = []
         self.code = []
-        
+
         self.filename = filename
         self.func = func
-    
+
+        # self.func_regex = r'^[ \t]*def[ \t]+{function_name!s}[ \t]*\(.*(,[ \t]*\n)*.*\)[ \t]*:'.format(function_name = func)
+
+        self.func_regex = r'^[ \t]*def[ \t]+{func_name!s}[ \t]*\(.*'.format(func_name = func)
+
         self.loadCode()
-    
+
     def loadCode(self):
         """ Loads the code from the file """
         lines = self.loadFile()
         funcStart = self.findFunc(lines)
-        
-        
+
+
         print "Function starts on line: %d" % funcStart
 
     def loadFile(self):
@@ -27,17 +31,17 @@ class MockFunc:
         except IOError:
             print "Unable to open file"
             exit(-1)
-            
+
         return file.readlines()
-        
+
     def findFunc(self, lines):
         for i in range(len(lines)):
             line = lines[i]
-            if not line.find("def "+ self.func) == -1:
+            matchObj = re.match(self.func_regex, line)
+            if matchObj:
                 print "YATAAA!!! We found it"
                 return i
-                
-                
+
+
 if __name__ == "__main__":
     m = MockFunc("test_func.py", "test_func")
-    
