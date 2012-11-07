164efba4c0e6e9cc6d2fafb819486025dbeeec72
Chris Loew <cloew123@gmail.com>
2012-11-06 21:54:29 -0700

    Console Helper can now process terminal metadata characters

--- src/console_helper.py
+++ src/console_helper.py
@@ -1,5 +1,5 @@
 import sys, tty, termios
-
+import select
 import curses
 
 ESCAPE = 27
@@ -19,17 +19,65 @@ DOWN_ARROW = 66
 RIGHT_ARROW = 67
 LEFT_ARROW = 68
 
+
+KAO_UP = 260
+KAO_DOWN = 261
+KAO_LEFT = 262
+KAO_RIGHT = 263
+KAO_DELETE = 264
+KAO_HOME = 265
+KAO_END = 266
+
+metaCharToKAO = {UP_ARROW:KAO_UP,
+                              DOWN_ARROW:KAO_DOWN,
+                              LEFT_ARROW:KAO_LEFT,
+                              RIGHT_ARROW:KAO_RIGHT,
+                              DELETE:KAO_DELETE,
+                              HOME:KAO_HOME,
+                              END:KAO_END
+                             }
+
 def getch():
     """ Retrieves a single character from the command line """
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
+        char = ord(sys.stdin.read(1))
+        metaChars = getMetaCharacters()
     finally:
         termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, old_settings)
-    return ch
+    return processMetaCharacter(char, metaChars)
     
 def cls():
     """ Clears the console """
     sys.stdout.write("\033c")
+    
+def getMetaCharacters():    
+        """ """
+        metaBytes = []
+        while True:
+            metaByte = getMetaCharacter()
+            if metaByte is None:
+                break
+            else:
+                metaBytes.append(metaByte)
+        return metaBytes
+
+def getMetaCharacter():  
+    """ Gets a Metadata Character """
+    metaByte = None
+    i,o,e = select.select([sys.stdin],[],[],0.0001)
+    for s in i:
+        if s == sys.stdin:
+            metaByte = ord(sys.stdin.read(1))
+    return metaByte
+    
+def processMetaCharacter(char, metaChars):
+    """ Processes an escaped meta-data character """
+    if char == ESCAPE and len(metaChars) > 0 and metaChars[0] == ARROW_ESCAPE:
+        if metaChars[1] in metaCharToKAO:
+            return metaCharToKAO[metaChars[1]]
+        return ord(" ")
+    return char
+        
--- src/input_processor.py
+++ src/input_processor.py
@@ -1,6 +1,6 @@
 from console_helper import *
 
-import curses
+import curses.ascii
 
 class InputProcessor:
     """ Processor of command line input """
@@ -10,55 +10,32 @@ class InputProcessor:
         self.debugging = debug
         self.addString = parent.addString
         self.cmds = {CTRL_S:parent.save, 
-                           ESCAPE:self.processEscape,
+                           ESCAPE:parent.exit,#self.processEscape,
                            ENDL:parent.addLine,
                            TAB:parent.addTab,
-                           BACKSPACE:parent.remove}
-                           
-                           
-        self.escapeCmds = {ESCAPE:parent.exit,
-                                     ARROW_ESCAPE:self.processArrowEscape}
-                                     
-        self.arrowCmds = {UP_ARROW:parent.cursorUp,
-                                   DOWN_ARROW:parent.cursorDown,
-                                   LEFT_ARROW:parent.cursorLeft,
-                                   RIGHT_ARROW:parent.cursorRight,
-                                   DELETE:parent.delete,
-                                   HOME:parent.cursorStart,
-                                   END:parent.cursorEnd}
+                           BACKSPACE:parent.remove,
+                           KAO_UP:parent.cursorUp,
+                           KAO_DOWN:parent.cursorDown,
+                           KAO_LEFT:parent.cursorLeft,
+                           KAO_RIGHT:parent.cursorRight,
+                           KAO_DELETE:parent.delete,
+                           KAO_HOME:parent.cursorStart,
+                           KAO_END:parent.cursorEnd}
         
     def processInput(self):
         """ Processes the command line input """
-        c, val = self.getInput()
+        val = self.getInput()
         
         if val in self.cmds.keys():
             self.cmds[val]()
-        else:
-            self.addString(c)
-            
-    def processEscape(self):
-        """ Processes an escape sequence """
-        c, val = self.getInput()
-        
-        if val in self.escapeCmds.keys():
-            self.escapeCmds[val]()
-        
-    def processArrowEscape(self):
-        """ Processes an Arrow Excape command """
-        c, val = self.getInput()
-        
-        if val == DELETE:
-                self.getInput()
-        
-        if val in self.arrowCmds.keys():
-            self.arrowCmds[val]()
+        elif curses.ascii.isprint(val):
+            self.addString(chr(val))
             
     def getInput(self):
         """ Gets a char and its ord """
-        c = getch()
-        val = ord(c)
+        val = getch()
         
         if self.debugging:
             print val, "\r"
         
-        return c, val
+        return val
