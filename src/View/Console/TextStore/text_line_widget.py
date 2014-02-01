c90e53c6f9045a3dd2a3b9004f3120b6e9bcf2c0
Chris Loew <cloew123@gmail.com>
2014-01-31 17:28:57 -0700

    Added Text Line Widget to display a single line

---
+++ src/View/Console/TextStore/text_line_widget.py
@@ -0,0 +1,21 @@
+from kao_gui.console.console_widget import ConsoleWidget
+
+class TextLineWidget(ConsoleWidget):
+    """ Represents the view for a Text Line Widget """
+    
+    def __init__(self, line, lineNumber, textWindow):
+        """ Initialize the view """
+        lineNumber = str(i)
+        self.lineNumber = lineNumber.zfill(maxLength)
+        self.line = line
+        
+        self.startCol = textWindow.left_col
+        self.endCol = textWindow.left_col + textWindow.window_width
+        
+    def draw(self):
+        """ Draw the Widget """
+        self.printTextLine(self.line)
+        
+    def printTextLine(self, line):
+        """  """
+        print "{0}: {1}{t.normal}\r".format(self.lineNumber, line, t=Window.terminal)
