4c7921eb1cf12bb2742a143b3f8795f4407c2339
Chris Loew <cloew123@gmail.com>
2014-02-13 19:52:17 -0700

    Added the Remove Character Operation

---
+++ src/Editor/TextStore/Operation/remove_character_operation.py
@@ -0,0 +1,12 @@
+from Editor.TextStore.Action.action_helper import concatenate
+from Editor.TextStore.Operation.text_store_operation import TextStoreOperation
+
+class RemoveCharacterOperation(TextStoreOperation):
+    """ Represents operation to remove a character """
+    
+    def perform(self):
+        """ Perform the Action """
+        textLine = self.textStore.text[self.cursor.line]
+        col = self.cursor.col
+        
+        self.textStore.text[self.cursor.line] = concatenate(textLine, col, col+1)
