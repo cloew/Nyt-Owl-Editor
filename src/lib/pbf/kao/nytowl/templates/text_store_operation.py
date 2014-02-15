1a573c02a6211b752437b828ccb2644225fdccb5
Chris Loew <cloew123@gmail.com>
2014-02-14 22:15:04 -0700

    Updated Text Store Operation template

--- src/lib/pbf/kao/nytowl/templates/text_store_operation.py
+++ src/lib/pbf/kao/nytowl/templates/text_store_operation.py
@@ -1,7 +1,7 @@
 from Editor.TextStore.Operation.text_store_operation import TextStoreOperation
 
 class %ClassName%(TextStoreOperation):
-    """ Represents operation to insert text """
+    """ Represents operation to ADD DESCRIPTION HERE """
     
     def perform(self):
         """ Perform the Action """
