157418aaf1dfec5fd53c72734f1240fd9c610be6
Chris Loew <cloew123@gmail.com>
2014-02-14 23:39:22 -0700

    Remove Previous Text Action now utilizes Remove Next Character

--- src/Editor/TextStore/Action/remove_previous_text_action.py
+++ src/Editor/TextStore/Action/remove_previous_text_action.py
@@ -1,40 +1,21 @@
 from Editor.TextStore.Action.merge_lines_action import MergeLinesAction
-from Editor.TextStore.Action.remove_character_action import RemoveCharacterAction
+from Editor.TextStore.Action.remove_next_text_action import RemoveNextTextAction
+from Editor.TextStore.Action.text_store_action import TextStoreAction
 
-class RemovePreviousTextAction:
+class RemovePreviousTextAction(TextStoreAction):
     """ Represents action to remove text """
-    
-    def __init__(self, cursor, textStore, event=None):
-        """ Initialize the Action """
-        self.line = cursor.line
-        self.column = cursor.col
-        self.cursor = cursor
-        self.textStore = textStore
         
     def do(self):
         """ Perform the action """
         if self.column == 0:
-            self.removeLine()
+            self.cursor.up()
+            self.cursor.toEndOfLine()
         else:
-            self.removeChar()
-        
-    def removeChar(self):
-        """ Removes a character from a line """
-        self.cursor.left()
-        
-        action = RemoveCharacterAction(self.cursor, self.textStore)
-        action.do()
-        
-    def removeLine(self):
-        """ Removes a line and appends the extra characters to the line above """
-        if self.line == 0:
-            return
-        
-        self.cursor.up()
-        self.cursor.toEndOfLine()
-        
-        action = MergeLinesAction(self.cursor, self.textStore)
-        action.do()
+            self.cursor.left()
+            
+        self.action = RemoveNextTextAction(self.cursor, self.textStore)
+        self.action.do()
         
     def undo(self):
         """ Undo the remove tab action """
+        self.action.undo()
