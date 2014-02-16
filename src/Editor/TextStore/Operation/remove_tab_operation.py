d3907e67dc80bca3a32953593dece537fe37cc75
Chris Loew <cloew123@gmail.com>
2014-02-16 09:30:10 -0700

    Remove Tab Operation can now perform on Hard Tabs

--- src/Editor/TextStore/Operation/remove_tab_operation.py
+++ src/Editor/TextStore/Operation/remove_tab_operation.py
@@ -1,3 +1,5 @@
+from Editor.Settings.settings import Settings
+
 from Editor.TextStore.Operation.remove_character_operation import RemoveCharacterOperation
 from Editor.TextStore.Operation.text_store_operation import TextStoreOperation
 
@@ -11,6 +13,19 @@ class RemoveTabOperation(TextStoreOperation):
     
     def perform(self):
         """ Perform the Action """
+        if self.settings.tabStyle is Settings.HARD_TABS:
+            self.removeHardTab()
+        else:
+            self.removeSoftTab()
+        
+    def removeHardTab(self):
+        """ Remove a tab """
+        self.cursor.left()
+        operation = RemoveCharacterOperation(self.cursor, self.textStore)
+        operation.perform()
+        
+    def removeSoftTab(self):
+        """ Remove a space-based tab """
         for i in range(self.settings.tabSize):
             self.cursor.left()
         operation = RemoveCharacterOperation(self.cursor, self.textStore)
